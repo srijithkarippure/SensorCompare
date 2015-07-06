7e3606c554022fa1e0131c88dade8ef5c91cf0fe
srijithkarippure <srijithk.91@gmail.com>
2015-07-06 23:25:33 +0530

    CheckStyle,Jlint,FindBugs Checks
    All checks have passed

--- src/com/sase/fcs/assignment1/Compare.java
+++ src/com/sase/fcs/assignment1/Compare.java
@@ -9,108 +9,157 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+
 /**
- * 
+ * Class for Comparing Sensor Data.
+ *
  * @author Srijith
- * Reads sensor data from file, populates to sensor objects and adds it to list
- * Compares Sensor data pair wise and finds the sensor with max and min temperature value
- * 
+ * Reads sensor data from file, populates
+ * sensor objects and adds it to list
+ * Compares Sensor data pair wise and
+ * finds the sensor with max and min temperature value
  */
 public class Compare implements Comparator<Sensor> {
-	Sensor maxSensor;
-	Sensor minSensor;
-	int i = 0;
-	public static void main(String[] args) throws IOException, ParseException {
+  /**
+   * temperature is stored as 4th column in the file.
+   * This value is used to retrieve temp value from
+   * file
+   */
+  private static final int SENSOR_TEMPERATURE_COLUMN = 3;
+  /**
+   * Each sensor must have 4 attributes.
+   */
+  private static final int REQUIRED_SENSOR_DETAILS = 4;
+  /**
+   * Sensor with max temperature.
+   */
+  private Sensor maxSensor;
+  /**
+   * Sensor with min temperature.
+   */
+  private Sensor minSensor;
+  /**
+   * Counter to count number of comparisons.
+   */
+  private static int i = 0;
 
-		Sensor s;
-		BufferedReader br = null;
-		try {
-			Compare c = new Compare();
-			br = new BufferedReader(new FileReader(new File("input.txt")));
-			List<Sensor> allSensors = new ArrayList<Sensor>();
-			int iter = 0;
-			while (br.ready()) {
+  /**
+   * Reads sensor data from file.
+   * populates sensor objects and stores
+   * in an array list. Compares
+   * temperature os sensors pair wise and
+   * finds sensors generating max and min temps
+   *
+   * @param args
+   *          Not used
+   * @throws IOException
+   *           File related exceptions
+   * @throws ParseException
+   *           File Related Exception
+   */
+  public static void main(final String[] args)
+      throws IOException, ParseException {
 
-				String sensorArray[] = br.readLine().split(",");
-				if (sensorArray.length != 4) {
-					System.out
-							.println("Invalid input, proceeding with the next input");
-					continue;
-				}
-				s = new Sensor();
-				s.setSensorId(sensorArray[0]);
-				s.setLocation(sensorArray[1]);
-				SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
-				s.setTimeStamp(fmt.parse(sensorArray[2]));
-				s.setTemperatureValue(Float.parseFloat(sensorArray[3]));
-				allSensors.add(s);
-			}
-			int totalNoOfSensors = allSensors.size();
-			if (totalNoOfSensors == 0) {
-				System.out
-						.println("No input provided, exiting the application");
-				System.exit(0);
-			} else {
-				Sensor firstSensor = allSensors.get(0);
-				if (totalNoOfSensors == 1) {
-					c.maxSensor = firstSensor;
-					c.minSensor = firstSensor;
-				} else {
-					if (totalNoOfSensors % 2 != 0) {
-						c.maxSensor = firstSensor;
-						c.minSensor = firstSensor;
-						iter = 1;
-					} else {
-						Sensor secondSensor = allSensors.get(1);
-						c.i++;
-						if (firstSensor.getTemperatureValue() > secondSensor
-								.getTemperatureValue()) {
-							c.maxSensor = firstSensor;
-							c.minSensor = secondSensor;
+    Sensor s;
+    BufferedReader br = null;
+    try {
+      Compare c = new Compare();
+      br = new BufferedReader(new FileReader(new File("input.txt")));
+      List<Sensor> allSensors = new ArrayList<Sensor>();
+      int iter = 0;
+      String readLine = br.readLine();
+      while (readLine != null) {
 
-						} else {
-							c.maxSensor = secondSensor;
-							c.minSensor = firstSensor;
-						}
-						iter = 2;
-					}
-				}
-			}
+        String[] sensorArray = readLine.split(",");
+        if (sensorArray.length != REQUIRED_SENSOR_DETAILS) {
+          System.out.println("Invalid input, proceeding with the next input");
+          continue;
+        }
+        s = new Sensor();
+        s.setSensorId(sensorArray[0]);
+        s.setLocation(sensorArray[1]);
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+        s.setTimeStamp(fmt.parse(sensorArray[2]));
+        s.setTemperatureValue(Float.parseFloat(
+            sensorArray[SENSOR_TEMPERATURE_COLUMN]));
+        allSensors.add(s);
+        readLine = br.readLine();
+      }
+      int totalNoOfSensors = allSensors.size();
+      if (totalNoOfSensors == 0) {
+        System.out.println("No input provided, exiting the application");
+        System.exit(0);
+      } else {
+        Sensor firstSensor = allSensors.get(0);
+        if (totalNoOfSensors == 1) {
+          c.maxSensor = firstSensor;
+          c.minSensor = firstSensor;
+        } else {
+          if (totalNoOfSensors % 2 != 0) {
+            c.maxSensor = firstSensor;
+            c.minSensor = firstSensor;
+            iter = 1;
+          } else {
+            Sensor secondSensor = allSensors.get(1);
+            Compare.i++;
+            if (firstSensor.getTemperatureValue()
+                > secondSensor.getTemperatureValue()) {
+              c.maxSensor = firstSensor;
+              c.minSensor = secondSensor;
 
-			while (iter < totalNoOfSensors - 1) {
-				c.compare(allSensors.get(iter), allSensors.get(iter + 1));
-				iter += 2;
-			}
-			System.out.println("Max Temp Value:"
-					+ c.maxSensor.getTemperatureValue());
-			System.out.println("Min Temp Value:"
-					+ c.minSensor.getTemperatureValue());
-			System.out.println("No of Comparisons: " + c.i);
-		} finally {
-			br.close();
-		}
-	}
+            } else {
+              c.maxSensor = secondSensor;
+              c.minSensor = firstSensor;
+            }
+            iter = 2;
+          }
+        }
+      }
 
-	public int compare(Sensor s1, Sensor s2) {
-		i++;
-		if (s1.getTemperatureValue() > s2.getTemperatureValue()) {
-			i++;
-			if (s1.getTemperatureValue() > maxSensor.getTemperatureValue()) {
-				maxSensor = s1;
-				i++;
-				if (s2.getTemperatureValue() < minSensor.getTemperatureValue())
-					minSensor = s2;
-			}
-		} else {
-			i++;
-			if (s2.getTemperatureValue() > maxSensor.getTemperatureValue()) {
-				maxSensor = s2;
-				i++;
-				if (s1.getTemperatureValue() < minSensor.getTemperatureValue())
-					minSensor = s1;
-			}
-		}
-		return 0;
-	}
-
+      while (iter < totalNoOfSensors - 1) {
+        c.compare(allSensors.get(iter), allSensors.get(iter + 1));
+        iter += 2;
+      }
+      System.out.println("Max Temp Value:" + c.maxSensor.getTemperatureValue());
+      System.out.println("Min Temp Value:" + c.minSensor.getTemperatureValue());
+      System.out.println("No of Comparisons: " + Compare.i);
+    } finally {
+      try {
+        br.close();
+      } catch (Exception e) {
+        System.out.println("Buffered Reader close Exception");
+      }
+    }
+  }
+  /**
+   * Compares two sensor objects and finds the min and max.
+   * Then compares the min with actual MinSensor
+   * and max with actual maxSensor and changes value accordingly
+   * @param s1 Sensor-1
+   * @param s2 Sensor-2
+   * @return 0 used for nothing
+   */
+  public final int compare(final Sensor s1, final Sensor s2) {
+    i++;
+    if (s1.getTemperatureValue() > s2.getTemperatureValue()) {
+      i++;
+      if (s1.getTemperatureValue() > maxSensor.getTemperatureValue()) {
+        maxSensor = s1;
+        i++;
+        if (s2.getTemperatureValue() < minSensor.getTemperatureValue()) {
+          minSensor = s2;
+        }
+      }
+    } else {
+      i++;
+      if (s2.getTemperatureValue() > maxSensor.getTemperatureValue()) {
+        maxSensor = s2;
+        i++;
+        if (s1.getTemperatureValue() < minSensor.getTemperatureValue()) {
+          minSensor = s1;
+        }
+      }
+    }
+    return 0;
+  }
 }
--- src/com/sase/fcs/assignment1/Sensor.java
+++ src/com/sase/fcs/assignment1/Sensor.java
@@ -2,35 +2,82 @@ package com.sase.fcs.assignment1;
 
 import java.util.Date;
 
+/**
+ * @author Srijith
+ * Sensor bean class with attributes
+ */
 public class Sensor {
-	String sensorId;
-	String location;
-	Date timeStamp;
-	float temperatureValue;
-	
-	public String getSensorId() {
-		return sensorId;
-	}
-	public void setSensorId(String sensorId) {
-		this.sensorId = sensorId;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	public Date getTimeStamp() {
-		return timeStamp;
-	}
-	public void setTimeStamp(Date timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-	public float getTemperatureValue() {
-		return temperatureValue;
-	}
-	public void setTemperatureValue(float temperatureValue) {
-		this.temperatureValue = temperatureValue;
-	}
-	
+  /**
+   * Sensor Id of the sensor.
+   */
+  private String sensorId;
+  /**
+   * Location at which the sensor is present.
+   */
+  private String location;
+  /**
+   * Time at which the temperature was noted.
+   */
+  private Date timeStamp;
+  /**
+   * Temperature emitted by the sensor.
+   */
+  private float temperatureValue;
+  /**
+   * Getter method for sensorId.
+   * @return SensorId
+   */
+  public final String getSensorId() {
+    return sensorId;
+  }
+  /**
+   * Setter method for SensorId.
+   * @param id Id of the sensor
+   */
+  public final void setSensorId(final String id) {
+    this.sensorId = id;
+  }
+  /**
+   * Getter for location attribute.
+   * @return location
+   */
+  public final String getLocation() {
+    return location;
+  }
+  /**
+   * setter for location.
+   * @param loc Location
+   */
+  public final void setLocation(final String loc) {
+    this.location = loc;
+  }
+  /**
+   * Getter for timestamp attribute.
+   * @return timestamp
+   */
+  public final Date getTimeStamp() {
+    return timeStamp;
+  }
+  /**
+   * Setter for timestamp.
+   * @param ts timestamp
+   */
+  public final void setTimeStamp(final Date ts) {
+    this.timeStamp = ts;
+  }
+  /**
+   * Getter for temperature.
+   * @return temperature
+   */
+  public final float getTemperatureValue() {
+    return temperatureValue;
+  }
+  /**
+   * Setter for temperature.
+   * @param tempValue Temperature
+   */
+  public final void setTemperatureValue(final float tempValue) {
+    this.temperatureValue = tempValue;
+  }
+
 }
